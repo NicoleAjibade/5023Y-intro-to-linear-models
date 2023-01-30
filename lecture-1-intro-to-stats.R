fc77d6c0e6ab3ec8779635ff7979882cd06286f1
NicoleAjibade <cuj20tku@uea.ac.uk>
2023-01-30 16:49:48 +0000

    Standard errors etc

--- lecture-1-intro-to-stats.R
+++ lecture-1-intro-to-stats.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
+library(kableExtra)
 
 darwin <- read_csv(here("data", "darwin.csv"))
 
@@ -69,3 +70,69 @@ darwin %>%
   group_by(type) %>% 
   summarise(mean=mean(height),
             sd=sd(height))
+
+# make a new object
+darwin_summary <-darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a summary plot
+darwin_summary %>% 
+  ggplot(aes(x=type,
+             y=mean))+
+  geom_pointrange(aes(ymin=mean-sd, ymax=mean+sd))+
+  theme_bw()
+
+# use kable extra functions to make a nice table (could be replaced with kable() if needed)
+darwin_summary %>% 
+  kbl(caption="Summary statistics of crossed and selfed maize plants") %>% 
+  kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
+
+# OTHER ----
+
+# pivot data to wide format then subtract Selfed plant heights from Crossed plant heights
+
+darwin_wide <- darwin %>% 
+  pivot_wider(names_from = type, values_from = height) %>% 
+  mutate(difference = Cross - Self)
+
+glimpse(darwin_wide)
+
+
+difference_summary <- darwin_wide %>% 
+  summarise(mean=mean(difference),
+            sd=sd(difference),
+            n=n())
+
+difference_summary
+
+# ADDING STANDARD ERROR ----
+
+difference_summary %>% 
+  mutate(se= sd/sqrt(n))
+
+## HISTOGRAMS ----
+
+#Create a sequence of 100 equally spaced numbers between -4 and 4
+x <- seq(-4, 4, length=100)
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x
+y <- dnorm(x)
+
+#plot x and y as a scatterplot with connected lines (type = "l") and add
+#an x-axis with custom labels
+plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
+axis(1, at = -3:3, labels = c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
+
+# CONFIDENCE INTERVALS ----
+
+lowerCI <- 2.62-(2*1.22)
+
+upperCI <- 2.62+(2*1.22)
+
+lowerCI
+upperCI
+
+
